7b6cf67c5f24f8572abf4ba13d8aaaee2695e3aa
chetan882777 <chetan882777@gmail.com>
2020-05-31 03:52:02 +0530

    set Activity

--- app/src/main/java/com/darshan/kotlin_coroutines/MainActivity.kt
+++ app/src/main/java/com/darshan/kotlin_coroutines/MainActivity.kt
@@ -2,11 +2,27 @@ package com.darshan.kotlin_coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel : MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        viewModel.user.observe(this, Observer { user ->
+            println(user)
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
     }
 }
